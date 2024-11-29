bc9132b8ee4fd24a596ae7964c055dd66a0fd732
Nintorch <92302738+Nintorch@users.noreply.github.com>
2024-11-29 11:09:31 +0500

    Subscribe to notifications

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import telebot
 
 with open("token.txt") as file:
     bot = telebot.TeleBot(file.read())
+users = set()
 
 
 @bot.message_handler(content_types=['text'])
 def get_text_messages(message):
     if message.text == "/start":
         bot.send_message(message.from_user.id, "Ожидайте уведомлений")
-
+        users.add(message.from_user.id)
+    elif message.text == "/stop":
+        bot.send_message(message.from_user.id, "Больше не ожидайте уведомлений :)")
+        users.remove(message.from_user.id)
 
 print("Bot is starting")
 bot.polling(none_stop=True, interval=0)
