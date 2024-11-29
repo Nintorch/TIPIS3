fcd895b64aea2fc85cf3a74cc6bfbb85ffd13192
Nintorch <92302738+Nintorch@users.noreply.github.com>
2024-11-29 12:01:26 +0500

    Send notifications

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from threading import Thread
 import telebot
 
 with open("token.txt") as file:
@@ -14,5 +15,14 @@ def get_text_messages(message):
         bot.send_message(message.from_user.id, "Больше не ожидайте уведомлений :)")
         users.remove(message.from_user.id)
 
+
+def wait_for_input():
+    while True:
+        text = input("Введите текст уведомления: ")
+        for user in users:
+            bot.send_message(user, "Уведомление: " + text, parse_mode="html")
+
+
 print("Bot is starting")
+Thread(target=wait_for_input).start()
 bot.polling(none_stop=True, interval=0)
